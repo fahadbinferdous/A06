4433430d5aa2b11619620ea298f0c71cd7c90c09
fahadbinferdous <bin.ferdous@gmail.com>
2026-04-01 19:10:43 +0600

    final v.2 -image path refined for netlify deployment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Play from "/src/assets/Play.png"
+import BannerImg from "/src/assets/banner.png"
 
 const Banner = () => {
     return (
         <div className='container mx-auto my-10 px-5 md:flex md:flex-row-reverse justify-between'>
             <div>
-                <img src="/src/assets/banner.png" alt="" />
+                <img src={BannerImg} alt="" />
             </div>
             <div className='flex flex-col justify-center items-start space-y-3'>
                 <p className='badge bg-linear-to-r from-[#4f39f621] to-[#9614fa18] text-primary rounded-full'>
@@ -17,7 +19,7 @@ const Banner = () => {
                 <div className='space-x-2'>
                     <button className='btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full'>Explore Products</button>
                     <button className='btn btn-primary btn-outline  rounded-full'>
-                        <img src="/src/assets/Play.png" alt="" />
+                        <img src={Play} alt="" />
                         Watch Demo
                     </button>
                 </div>           
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import User from "../../src/assets/user.png"
+import Package from "../../src/assets/package.png"
+import Rocket from "../../src/assets/rocket.png"
 
 const GetStarted = () => {
     return (
@@ -8,19 +11,19 @@ const GetStarted = () => {
             <div className='container mx-auto grid md:grid-cols-3 justify-between gap-5'>
                 <div className='flex flex-col items-center justify-center space-y-5 bg-white rounded-xl p-10 relative'>
                     <p className='badge bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white p-1 rounded-[50%] absolute top-3 right-3'>01</p>
-                    <img src="../../src/assets/user.png" alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f] p-3 rounded-[50%]' />
+                    <img src={User} alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f] p-3 rounded-[50%]' />
                     <h4 className='text-2xl font-bold text-center '>Create Account</h4>
                     <p className='text-[#627382] text-center'>Sign up for free in seconds. No credit card required to get started.</p>
                 </div>
                 <div className='flex flex-col items-center justify-center space-y-5 bg-white rounded-xl p-10 relative'>
                     <p className='badge bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white p-1 rounded-[50%] absolute top-3 right-3'>02</p>
-                    <img src="../../src/assets/package.png" alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f] p-3 rounded-[50%]' />
+                    <img src={Package} alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f] p-3 rounded-[50%]' />
                     <h4 className='text-2xl font-bold text-center '>Choose Products</h4>
                     <p className='text-[#627382] text-center'>Browse our catalog and select the tools that fit your needs.</p>
                 </div>
                 <div className='flex flex-col items-center justify-center space-y-5 bg-white rounded-xl p-10 relative'>
                     <p className='badge bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white p-1 rounded-[50%] absolute top-3 right-3'>03</p>
-                    <img src="../../src/assets/rocket.png" alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f]  p-3 rounded-[50%] ' />
+                    <img src={Rocket} alt="" className='bg-linear-to-r from-[#4f39f62f] to-[#9614fa2f]  p-3 rounded-[50%] ' />
                     <h4 className='text-2xl font-bold text-center '>Start Creating</h4>
                     <p className='text-[#627382] text-center'>Download and start using your premium tools immediately.</p>
                 </div>
